3c0b68a1fc5a6bef949ccd74f18252fa2dd329d0
Julian Maynes-Desktop <thelastpocket@gmail.com>
2024-06-22 01:17:05 -0700

    Made some more advancements with the chip and was introduced to sdl

---
+++ src/chip8.cpp
@@ -0,0 +1,71 @@
+#include <cstdint>
+//#include <iostream>
+#include <SDL2/SDL.h>
+
+class chip8{
+    public: 
+        //memory
+        uint8_t memory[4096]; // 4KB memory 
+        //registers
+        uint8_t registers[16];
+        uint16_t I_reg;
+        uint8_t delayTimer;
+        uint8_t soundTimer;
+
+        uint16_t PC; // Program Counter, points to currently executing address
+        uint8_t SP;  //Stack pointer, points to topmost level of stack
+        //stack
+        uint16_t stack[16];
+        
+        const uint8_t sprite_0[5] = {0xF0, 0x90,0x90,0x90, 0xF0};
+        const uint8_t sprite_1[5] = {0x20, 0x60,0x20, 0x20, 0x70};
+        const uint8_t sprite_2[5] = {0xF0, 0x10,0xF0, 0x80, 0xF0};
+        const uint8_t sprite_3[5] = {0xF0, 0x10,0xF0, 0x10, 0xF0};
+        const uint8_t sprite_4[5] = {0x90, 0x90,0xF0, 0x10, 0x10};
+        const uint8_t sprite_5[5] = {0xF0, 0x80,0xF0, 0x10, 0xF0};
+        const uint8_t sprite_6[5] = {0xF0, 0x80,0xF0, 0x90, 0xF0};
+        const uint8_t sprite_7[5] = {0xF0, 0x10,0x20, 0x40, 0x40};
+        const uint8_t sprite_8[5] = {0xF0, 0x90,0xF0, 0x90, 0xF0};
+        const uint8_t sprite_9[5] = {0xF0, 0x90,0xF0, 0x10, 0xF0};
+        const uint8_t sprite_A[5] = {0xF0, 0x90,0xF0, 0x90, 0x90};
+        const uint8_t sprite_B[5] = {0xE0, 0x90,0xE0, 0x90, 0xE0};
+        const uint8_t sprite_C[5] = {0xF0, 0x80,0x80, 0x80, 0xF0};
+        const uint8_t sprite_D[5] = {0xE0, 0x90,0x90, 0x90, 0xE0};
+        const uint8_t sprite_E[5] = {0xF0, 0x80,0xF0, 0x80, 0xF0};
+        const uint8_t sprite_F[5] = {0xF0, 0x80,0xF0, 0x80, 0x80};
+
+    void initializeMemory(){ // populates designated memory spaces 
+        for(int i = 0; i < 5; i++){
+            memory[i+80] = sprite_0[i];
+            memory[i+85] = sprite_1[i];
+            memory[i+90] = sprite_2[i];
+            memory[i+95] = sprite_3[i];
+            memory[i+100] = sprite_4[i];
+            memory[i+105] = sprite_5[i];
+            memory[i+110] = sprite_6[i];
+            memory[i+115] = sprite_7[i];
+            memory[i+120] = sprite_8[i];
+            memory[i+125] = sprite_9[i];
+            memory[i+130] = sprite_A[i];
+            memory[i+135] = sprite_B[i];
+            memory[i+140] = sprite_C[i];
+            memory[i+145] = sprite_D[i];
+            memory[i+150] = sprite_E[i];
+            memory[i+155] = sprite_F[i];
+        }
+        0x50;
+        0x9F;
+
+    }
+
+    void 00E0(){  //CLR, Clear display
+
+    }
+
+    void 00EE(){//RET, return from subroutine
+    //set program counter to the top opf stack , subtracts one from stack pointer
+
+    }
+
+    void 
+}
---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <iostream>
+
+const int SCREEN_WIDTH = 64;
+const int SCREEN_HEIGHT = 32;
+
+int main(int argc, char *argv[])
+{
+   SDL_Window * window = nullptr;
+
+    SDL_Surface * screenSurface = nullptr;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow("Chippy", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    screenSurface = SDL_GetWindowSurface(window);
+
+    //Fill surface wqite
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+
+    SDL_UpdateWindowSurface(window);
+
+    //Hack to get window to stay up
+    SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
