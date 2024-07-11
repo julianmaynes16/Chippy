b232eaa2c87fb0670a9094a769daf797cefa0a00
Julian Maynes <80068880+julianmaynes16@users.noreply.github.com>
2024-07-10 21:15:20 -0700

    Doing more debugging!

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,8 @@ int main(int argc, char *argv[])
     while(!interface.quit){
         interface.updateKeyboard();
         chip8.execute(&interface);
-        //interface.clearKeyboard();
+        
         interface.displayScreen(&chip8);
-        //chip8.debug_printRegisters();
-        //chip8.debug_printScreen();
-        //interface.debug_displayKeyboard();
         interface.clearKeyboard();
     }
     chip8.debug_printMemory();
